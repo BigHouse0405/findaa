94bafc939f4ed138dacebc4549cc6520fa8c74b2
BigHouse0405 <kubroma219@gmail.com>
2023-05-02 16:16:35 +0200

    [impl-2] Search user by fullname

--- src/main/java/com/bntech/qrekru/data/repository/UserRepository.java
+++ src/main/java/com/bntech/qrekru/data/repository/UserRepository.java
@@ -8,5 +8,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long>, JpaRepository<User, Long> {
-
+    User findByPwd(String pwd);
+    User findByFullName(String name);
 }
