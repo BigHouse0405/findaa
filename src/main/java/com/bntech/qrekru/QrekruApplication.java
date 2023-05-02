9349633ad782f7167698a90e82cfaf20c248826e
BigHouse0405 <kubroma219@gmail.com>
2023-05-02 11:06:48 +0200

    Initialise and configure project

---
+++ src/main/java/com/bntech/qrekru/QrekruApplication.java
@@ -0,0 +1,13 @@
+package com.bntech.qrekru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrekruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrekruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bntech/qrekru/QrekruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bntech.qrekru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrekruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
